b336f7352f5ed2320861f19802266dfb57dcd549
Vlad Crisan <vlad@kaiko.ai>
2023-02-22 09:49:03 +0100

    added imports in init, fixed imports, ran black

--- ahcore/utils/manifest.py
+++ ahcore/utils/manifest.py
@@ -275,6 +275,7 @@ def image_manifest_to_dataset(
         labels=labels,
         transform=transform,
         backend=image_backend,
+        limit_bounds=True if rois is None else False,
         **kwargs,
     )
 
