37d9b692a4d4fc637a8487c94559ecca80274454
Ajey Pai K <ajeypie@gmail.com>
2023-04-13 11:17:32 +0200

    Refactoring

--- ahcore/transforms/image_normalization.py
+++ ahcore/transforms/image_normalization.py
@@ -173,80 +173,6 @@ class MacenkoNormalizer(nn.Module):
         self._he_reference = self.HE_REFERENCE
         self._max_con_reference = self.MAX_CON_REFERENCE
 
-    def convert_rgb_to_optical_density(self, image_tensor: torch.Tensor) -> tuple[torch.Tensor, list[torch.Tensor]]:
-        """
-        This function converts an RGB image to optical density values following the Beer-Lambert's law.
-        Parameters
-        ----------
-        image_tensor: torch.Tensor
-            RGB image tensor, shape (B, 3, H, W)
-        Returns
-        -------
-        optical_density: torch.Tensor
-            Optical density of the image tensor, shape (B, H*W, 3)
-        optical_density_hat: list[torch.Tensor]
-            Optical density of the image tensor, shape (B, num_foreground_pixels, 3)
-        """
-        image_tensor = image_tensor.permute(0, 2, 3, 1).contiguous()
-        num_tiles, height, width, channels = image_tensor.shape
-        # calculate optical density
-        optical_density = -torch.log((image_tensor.to(torch.float64) + 1) / self._transmitted_intensity)
-        # remove transparent pixels
-        mask = optical_density.min(dim=-1).values > self._beta
-        optical_density_hat = [optical_density[i][mask[i]] for i in range(num_tiles)]
-        return optical_density, optical_density_hat
-
-    def convert_optical_density_to_rgb(self, od_tensor: torch.Tensor) -> torch.Tensor:
-        """
-        Converts optical density values to RGB
-        Parameters
-        ----------
-        od_tensor: torch.Tensor
-            Optical density of the image.
-        Returns
-        -------
-        rgb_tensor: torch.Tensor
-            RGB image.
-        """
-        projection_to_reference_stains = -self._he_reference.to(od_tensor) @ od_tensor
-        optical_density_to_rgb = torch.exp(projection_to_reference_stains)
-        normalised_image_tensor = optical_density_to_rgb * self._transmitted_intensity
-        normalised_image_tensor[normalised_image_tensor > 255.] = 255.
-        return normalised_image_tensor
-
-    def _find_he_components(self, optical_density_hat: torch.Tensor, eigvecs: torch.Tensor) -> torch.Tensor:
-        """
-        This function -
-        1. Computes the H&E staining vectors by projecting the OD values of the image pixels on the plane
-        spanned by the eigenvectors corresponding to their two largest eigenvalues.
-        2. Normalizes the staining vectors to unit length.
-        3. Calculates the angle between each of the projected points and the first principal direction.
-        Parameters:
-        ----------
-        optical_density_hat: torch.Tensor
-            Optical density of the image
-        eigvecs: torch.Tensor
-            Eigenvectors of the covariance matrix
-        Returns:
-        -------
-        he_components: torch.Tensor
-            The H&E staining vectors
-        """
-        t_hat = torch.matmul(optical_density_hat, eigvecs)
-        phi = torch.atan2(t_hat[:, 1], t_hat[:, 0])
-        min_phi = percentile(phi, self._alpha)
-        max_phi = percentile(phi, 100 - self._alpha)
-
-        v_min = torch.matmul(eigvecs, torch.stack((torch.cos(min_phi), torch.sin(min_phi)))).unsqueeze(1)
-        v_max = torch.matmul(eigvecs, torch.stack((torch.cos(max_phi), torch.sin(max_phi)))).unsqueeze(1)
-
-        # a heuristic to make the vector corresponding to hematoxylin first and the one corresponding to eosin second
-        he_vector = torch.where(
-            v_min[0] > v_max[0], torch.cat((v_min, v_max), dim=1), torch.cat((v_max, v_min), dim=1)
-        )
-
-        return he_vector
-
     def __compute_matrices(
             self, image_tensor: torch.Tensor, staining_parameters: dict[str: torch.Tensor]
     ) -> tuple(torch.Tensor, torch.Tensor):
@@ -270,51 +196,6 @@ class MacenkoNormalizer(nn.Module):
             he_concentrations, max_concentrations = _compute_concentrations(he, od_tensor)
             batch_con_vecs.append(he_concentrations)
         return torch.stack(batch_con_vecs, dim=0)
-
-    def fit(self, wsi: torch.Tensor, wsi_name: str, dump_to_folder: Optional[Path] = None) -> dict[str: torch.Tensor]:
-        """
-        Compress a WSI to a single matrix of eigenvectors and return staining parameters.
-        Parameters:
-        ----------
-        wsi: torch.tensor
-            A tensor containing a whole slide image of shape (1, channels, height, width)
-        name: Path
-            Path to the WSI file
-        Returns:
-        -------
-        staining_parameters: dict[str: torch.Tensor, str: torch.Tensor]
-            The eigenvectors of the optical density values of the pixels in the image.
-        Note:
-            Dimensions of HE vector are: (3 x 2)
-            Dimensions of max cancentration vector are: (2)
-            Dimensions of wsi_eigenvectors are: (3 x 2)
-        """
-        logger.info("Fitting stain matrix for WSI: %s", wsi_name)
-        optical_density, optical_density_hat = self.convert_rgb_to_optical_density(wsi)
-        optical_density = optical_density.squeeze(0).view(-1, 3)
-        wsi_eigenvectors = _compute_eigenvecs(optical_density_hat[0])
-        wsi_level_he = self._find_he_components(optical_density_hat[0], wsi_eigenvectors)
-        wsi_level_concentrations, wsi_level_max_concentrations = _compute_concentrations(wsi_level_he, optical_density)
-        staining_parameters = {
-            "wsi_name": wsi_name,
-            "wsi_staining_vectors": wsi_level_he.unsqueeze(0),
-            "max_wsi_concentration": wsi_level_max_concentrations.unsqueeze(0).unsqueeze(0),
-        }
-        if dump_to_folder:
-            dump_staining_parameters(staining_parameters, dump_to_folder)
-        return staining_parameters
-
-    def set(self, target_image: torch.Tensor) -> None:
-        """
-        Set the reference image for the stain normaliser.
-        Parameters:
-        ----------
-        target_image: torch.Tensor
-            The reference image for the stain normaliser.
-        """
-        staining_parameters = self.fit(wsi=target_image, wsi_name="target image")
-        self._he_reference = staining_parameters["wsi_staining_vectors"]
-        self._max_con_reference = staining_parameters["max_wsi_concentration"]
 
     def __normalize_concentrations(
             self, concentrations: torch.Tensor, maximum_concentration: torch.tensor
@@ -391,6 +272,125 @@ class MacenkoNormalizer(nn.Module):
         h_stain = _handle_stain_tensors(h_stain, image_tensor.shape)
         e_stain = _handle_stain_tensors(e_stain, image_tensor.shape)
         return h_stain, e_stain
+
+    def _find_he_components(self, optical_density_hat: torch.Tensor, eigvecs: torch.Tensor) -> torch.Tensor:
+        """
+        This function -
+        1. Computes the H&E staining vectors by projecting the OD values of the image pixels on the plane
+        spanned by the eigenvectors corresponding to their two largest eigenvalues.
+        2. Normalizes the staining vectors to unit length.
+        3. Calculates the angle between each of the projected points and the first principal direction.
+        Parameters:
+        ----------
+        optical_density_hat: torch.Tensor
+            Optical density of the image
+        eigvecs: torch.Tensor
+            Eigenvectors of the covariance matrix
+        Returns:
+        -------
+        he_components: torch.Tensor
+            The H&E staining vectors
+        """
+        t_hat = torch.matmul(optical_density_hat, eigvecs)
+        phi = torch.atan2(t_hat[:, 1], t_hat[:, 0])
+        min_phi = percentile(phi, self._alpha)
+        max_phi = percentile(phi, 100 - self._alpha)
+
+        v_min = torch.matmul(eigvecs, torch.stack((torch.cos(min_phi), torch.sin(min_phi)))).unsqueeze(1)
+        v_max = torch.matmul(eigvecs, torch.stack((torch.cos(max_phi), torch.sin(max_phi)))).unsqueeze(1)
+
+        # a heuristic to make the vector corresponding to hematoxylin first and the one corresponding to eosin second
+        he_vector = torch.where(
+            v_min[0] > v_max[0], torch.cat((v_min, v_max), dim=1), torch.cat((v_max, v_min), dim=1)
+        )
+
+        return he_vector
+
+    def convert_rgb_to_optical_density(self, image_tensor: torch.Tensor) -> tuple[torch.Tensor, list[torch.Tensor]]:
+        """
+        This function converts an RGB image to optical density values following the Beer-Lambert's law.
+        Parameters
+        ----------
+        image_tensor: torch.Tensor
+            RGB image tensor, shape (B, 3, H, W)
+        Returns
+        -------
+        optical_density: torch.Tensor
+            Optical density of the image tensor, shape (B, H*W, 3)
+        optical_density_hat: list[torch.Tensor]
+            Optical density of the image tensor, shape (B, num_foreground_pixels, 3)
+        """
+        image_tensor = image_tensor.permute(0, 2, 3, 1).contiguous()
+        num_tiles, height, width, channels = image_tensor.shape
+        # calculate optical density
+        optical_density = -torch.log((image_tensor.to(torch.float64) + 1) / self._transmitted_intensity)
+        # remove transparent pixels
+        mask = optical_density.min(dim=-1).values > self._beta
+        optical_density_hat = [optical_density[i][mask[i]] for i in range(num_tiles)]
+        return optical_density, optical_density_hat
+
+    def convert_optical_density_to_rgb(self, od_tensor: torch.Tensor) -> torch.Tensor:
+        """
+        Converts optical density values to RGB
+        Parameters
+        ----------
+        od_tensor: torch.Tensor
+            Optical density of the image.
+        Returns
+        -------
+        rgb_tensor: torch.Tensor
+            RGB image.
+        """
+        projection_to_reference_stains = -self._he_reference.to(od_tensor) @ od_tensor
+        optical_density_to_rgb = torch.exp(projection_to_reference_stains)
+        normalised_image_tensor = optical_density_to_rgb * self._transmitted_intensity
+        normalised_image_tensor[normalised_image_tensor > 255.] = 255.
+        return normalised_image_tensor
+
+    def fit(self, wsi: torch.Tensor, wsi_name: str, dump_to_folder: Optional[Path] = None) -> dict[str: torch.Tensor]:
+        """
+        Compress a WSI to a single matrix of eigenvectors and return staining parameters.
+        Parameters:
+        ----------
+        wsi: torch.tensor
+            A tensor containing a whole slide image of shape (1, channels, height, width)
+        name: Path
+            Path to the WSI file
+        Returns:
+        -------
+        staining_parameters: dict[str: torch.Tensor, str: torch.Tensor]
+            The eigenvectors of the optical density values of the pixels in the image.
+        Note:
+            Dimensions of HE vector are: (3 x 2)
+            Dimensions of max cancentration vector are: (2)
+            Dimensions of wsi_eigenvectors are: (3 x 2)
+        """
+        logger.info("Fitting stain matrix for WSI: %s", wsi_name)
+        optical_density, optical_density_hat = self.convert_rgb_to_optical_density(wsi)
+        optical_density = optical_density.squeeze(0).view(-1, 3)
+        wsi_eigenvectors = _compute_eigenvecs(optical_density_hat[0])
+        wsi_level_he = self._find_he_components(optical_density_hat[0], wsi_eigenvectors)
+        wsi_level_concentrations, wsi_level_max_concentrations = _compute_concentrations(wsi_level_he, optical_density)
+        staining_parameters = {
+            "wsi_name": wsi_name,
+            "wsi_staining_vectors": wsi_level_he.unsqueeze(0),
+            "max_wsi_concentration": wsi_level_max_concentrations.unsqueeze(0).unsqueeze(0),
+        }
+        if dump_to_folder:
+            dump_staining_parameters(staining_parameters, dump_to_folder)
+        return staining_parameters
+
+    def set(self, target_image: torch.Tensor) -> None:
+        """
+        Set the reference image for the stain normaliser.
+        Parameters:
+        ----------
+        target_image: torch.Tensor
+            The reference image for the stain normaliser.
+        """
+        staining_parameters = self.fit(wsi=target_image, wsi_name="target image")
+        self._he_reference = staining_parameters["wsi_staining_vectors"]
+        self._max_con_reference = staining_parameters["max_wsi_concentration"]
 
     def forward(self, *args: tuple[torch.Tensor], **kwargs) -> list[torch.Tensor]:
         output = []
