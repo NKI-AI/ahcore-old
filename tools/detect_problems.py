5d9d895c86a92b1f762eb215f0d7e323c7f11052
Jonas Teuwen <j.teuwen@nki.nl>
2023-08-24 13:14:29 +0200

    Use multiprocessing

--- tools/detect_problems.py
+++ tools/detect_problems.py
@@ -1,16 +1,28 @@
-from pathlib import Path
+# encoding: utf-8
+import argparse
 import json
+import multiprocessing
+
 import tqdm
-from multiprocessing import Pool, cpu_count
 
-# TODO: Make much more elaborate, check each file!
+from ahcore.cli import dir_path
 
 
-def process_svs_folder(svs_folder):
+def is_jpeg(filename):
+    with open(filename, "rb") as f:
+        start_bytes = f.read(2)
+        f.seek(-2, 2)
+        end_bytes = f.read(2)
+    return start_bytes == b"\xFF\xD8" and end_bytes == b"\xFF\xD9"
+
+
+def process_svs_folder(args):
+    svs_folder, lock = args
     metadata_fn = svs_folder / "meta_data_original_slide.json"
     if not metadata_fn.is_file():
-        with open("errors.txt", "a") as f:
-            f.write(str(svs_folder) + ",missing_metadata\n")
+        with lock:
+            with open("errors.txt", "a") as f:
+                f.write(f"{svs_folder},missing_metadata\n")
         return
 
     with open(svs_folder / "meta_data_tiles.json") as json_file:
@@ -18,18 +30,49 @@ def process_svs_folder(svs_folder):
         max_images = meta_data_tiles["num_regions_masked"]
         tiles_dict = meta_data_tiles["tiles"]
         for i in range(max_images):
-            if (f"img_{i}.jpg" not in tiles_dict) or not (svs_folder/ f"img_{i}.jpg").is_file():
-                with open("errors.txt", "a") as f:
-                    f.write(str(svs_folder) + ",metadata\n")
+            if (f"img_{i}.jpg" not in tiles_dict) or not (svs_folder / f"img_{i}.jpg").is_file():
+                with lock:
+                    with open("errors.txt", "a") as f:
+                        f.write(f"{svs_folder},metadata\n")
                 return
+            else:
+                if not is_jpeg(svs_folder / f"img_{i}.jpg"):
+                    with lock:
+                        with open("errors.txt", "a") as f:
+                            f.write(f"{svs_folder},broken_jpeg\n")
+                    return
 
-def construct_tile_dict():
-    root_folder = Path("/projects/tcga_tiled/v1/data")
+                    
+def detect_problems():
+    parser = argparse.ArgumentParser("Tiling of whole slide images.")
+    # Assume a comma separated format from image_file,mask_file
+    parser.add_argument(
+        "ROOT",
+        type=dir_path,
+        help="Path to the output folder",
+    )
+    parser.add_argument(
+        "--num-workers",
+        type=int,
+        default=0,
+    )
+    args = parser.parse_args()
+
+    root_folder = args.ROOT
     svs_folders = list(root_folder.glob("*/*.svs"))
 
-    for svs_folder in tqdm.tqdm(svs_folders):
-        process_svs_folder(svs_folder)
+    if args.num_workers == 0:
+        for folder in tqdm.tqdm(svs_folders):
+            process_svs_folder((folder, None))
+    else:
+        lock = multiprocessing.Manager().Lock()
+        with multiprocessing.Pool(args.num_workers) as pool:
+            list(
+                tqdm.tqdm(
+                    pool.imap(process_svs_folder, [(folder, lock) for folder in svs_folders]), total=len(svs_folders)
+                )
+            )
 
 
 if __name__ == "__main__":
-    construct_tile_dict()
+    detect_problems()
