785a104cbfc8a55c54e0d0e26a5f4ddef099786d
Jonas Teuwen <j.teuwen@nki.nl>
2023-09-06 12:55:48 +0200

    use ORM

--- ahcore/utils/manifest_database.py
+++ ahcore/utils/manifest_database.py
@@ -1,271 +1,184 @@
 # encoding: utf-8
-import sqlite3
-from pathlib import Path
-
 import random
-from pydantic import BaseModel
+from enum import Enum as PyEnum
+
+from sqlalchemy import Column, DateTime, Enum, ForeignKey, Integer, String, create_engine, func
+from sqlalchemy.orm import DeclarativeBase, relationship, sessionmaker
 
 
-class _BaseModel(BaseModel):
+class CategoryEnum(PyEnum):
+    TRAIN = "train"
+    VALIDATE = "validate"
+    TEST = "test"
+
+
+class Base(DeclarativeBase):
     pass
 
-class Manifest(_BaseModel):
-    id: int
-    name: str
+
+class Manifest(Base):
+    __tablename__ = "manifest"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
+    created = Column(DateTime(timezone=True), default=func.now())
+    last_updated = Column(DateTime(timezone=True), default=func.now(), onupdate=func.now())
+
+    patients = relationship("Patient", back_populates="manifest")
 
 
-class Patient(_BaseModel):
-    id: int
-    patient_code: str
-    manifest_id: int
+class Patient(Base):
+    __tablename__ = "patient"
+    id = Column(Integer, primary_key=True)
+    patient_code = Column(String, unique=True)
+    manifest_id = Column(Integer, ForeignKey("manifest.id"))
 
-class Image(_BaseModel):
-    id: int
-    filename: str
-    reader: str
-    patient_id: int
-
-
-class Mask(_BaseModel):
-    id: int
-    filename: str
-    reader: str
-    image_id: int
+    manifest = relationship("Manifest", back_populates="patients")
+    images = relationship("Image", back_populates="patient")
+    labels = relationship("PatientLabels", back_populates="patient")
+    split = relationship("Split", uselist=False, back_populates="patient")
 
 
-class ImageAnnotations(_BaseModel):
-    id: int
-    filename: str
-    reader: str
-    image_id: int
+class Image(Base):
+    __tablename__ = "image"
+    id = Column(Integer, primary_key=True)
+    filename = Column(String)
+    reader = Column(String)
+    patient_id = Column(Integer, ForeignKey("patient.id"))
+
+    patient = relationship("Patient", back_populates="images")
+    masks = relationship("Mask", back_populates="image")
+    annotations = relationship("ImageAnnotations", back_populates="image")
+    labels = relationship("ImageLabels", back_populates="image")
 
 
-class ImageLabels(_BaseModel):
-    id: int
-    key: str
-    value: str
-    image_id: int
+class Mask(Base):
+    __tablename__ = "mask"
+    id = Column(Integer, primary_key=True)
+    filename = Column(String)
+    reader = Column(String)
+    image_id = Column(Integer, ForeignKey("image.id"))
 
-class PatientLabels(_BaseModel):
-    id: int
-    key: str
-    value: str
-    patient_id: int
-
-class SplitDefinitions(_BaseModel):
-    id: int
-    version: str
-    description: str
+    image = relationship("Image", back_populates="masks")
 
 
-class Split(_BaseModel):
-    id: int
-    patient_id: int
-    split_definition_id: int
-    category: str
+class ImageAnnotations(Base):
+    __tablename__ = "image_annotations"
+    id = Column(Integer, primary_key=True)
+    annotation_data = Column(String)  # Simplified for example, you might want to use a more complex datatype
+    image_id = Column(Integer, ForeignKey("image.id"))
+
+    image = relationship("Image", back_populates="annotations")
 
 
-class DatabaseManager:
-    def __init__(self, db_name: Path):
-        self.db_name = db_name
+class ImageLabels(Base):
+    __tablename__ = "image_labels"
+    id = Column(Integer, primary_key=True)
+    label_data = Column(String)  # e.g. "cancer" or "benign"
+    image_id = Column(Integer, ForeignKey("image.id"))
 
-    def _connect(self) -> sqlite3.Connection:
-        connection = sqlite3.connect(self.db_name)
-        # Enabling foreign key constraints
-        connection.execute("PRAGMA foreign_keys = ON;")
-        return connection
-
-    def create_tables(self) -> None:
-        with self._connect() as connection:
-            cursor = connection.cursor()
-
-            # Table creations
-            cursor.execute("""
-                CREATE TABLE IF NOT EXISTS manifest (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    name TEXT UNIQUE,
-                    created TEXT DEFAULT CURRENT_TIMESTAMP,
-                    last_updated TEXT DEFAULT CURRENT_TIMESTAMP
-                );
-            """)
-
-            cursor.execute("""
-                CREATE TABLE IF NOT EXISTS patient (
-                    id INTEGER PRIMARY KEY,
-                    patient_code TEXT UNIQUE NOT NULL,
-                    manifest_id INTEGER,
-                    created TEXT DEFAULT CURRENT_TIMESTAMP,
-                    last_updated TEXT DEFAULT CURRENT_TIMESTAMP,
-                    FOREIGN KEY (manifest_id) REFERENCES manifest (id)
-                );
-            """)
-
-            cursor.execute("""
-                CREATE TABLE IF NOT EXISTS image (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    filename TEXT,
-                    reader TEXT,
-                    patient_id INTEGER,
-                    created TEXT DEFAULT CURRENT_TIMESTAMP,
-                    last_updated TEXT DEFAULT CURRENT_TIMESTAMP,
-                    FOREIGN KEY (patient_id) REFERENCES patient (id)
-                );
-            """)
-
-            cursor.execute("""
-                CREATE TABLE IF NOT EXISTS mask (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    filename TEXT,
-                    reader TEXT,
-                    image_id INTEGER,
-                    created TEXT DEFAULT CURRENT_TIMESTAMP,
-                    last_updated TEXT DEFAULT CURRENT_TIMESTAMP,
-                    FOREIGN KEY (image_id) REFERENCES image (id)
-                );
-            """)
-
-            cursor.execute("""
-                CREATE TABLE IF NOT EXISTS image_annotations (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    filename TEXT,
-                    reader TEXT,
-                    image_id INTEGER,
-                    created TEXT DEFAULT CURRENT_TIMESTAMP,
-                    last_updated TEXT DEFAULT CURRENT_TIMESTAMP,
-                    FOREIGN KEY (image_id) REFERENCES image (id)
-                );
-            """)
-
-            cursor.execute("""
-                CREATE TABLE IF NOT EXISTS image_labels (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    key TEXT,
-                    value TEXT,
-                    image_id INTEGER,
-                    created TEXT DEFAULT CURRENT_TIMESTAMP,
-                    last_updated TEXT DEFAULT CURRENT_TIMESTAMP,
-                    FOREIGN KEY (image_id) REFERENCES image (id)
-                );
-            """)
-
-            cursor.execute("""
-                CREATE TABLE IF NOT EXISTS patient_labels (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    key TEXT,
-                    value TEXT,
-                    patient_id INTEGER,
-                    created TEXT DEFAULT CURRENT_TIMESTAMP,
-                    last_updated TEXT DEFAULT CURRENT_TIMESTAMP,
-                    FOREIGN KEY (patient_id) REFERENCES patient (id)
-                );
-            """)
-
-            cursor.execute("""
-                CREATE TABLE IF NOT EXISTS split_definitions (
-                    id INTEGER PRIMARY KEY,
-                    version TEXT NOT NULL,
-                    description TEXT,
-                    created TEXT DEFAULT CURRENT_TIMESTAMP,
-                    last_updated TEXT DEFAULT CURRENT_TIMESTAMP
-                );
-            """)
-
-            cursor.execute("""
-                CREATE TABLE IF NOT EXISTS split (
-                    id INTEGER PRIMARY KEY,
-                    patient_id INTEGER NOT NULL,
-                    split_definition_id INTEGER NOT NULL,
-                    category TEXT NOT NULL CHECK (category IN ('train', 'test', 'validate')),
-                    created TEXT DEFAULT CURRENT_TIMESTAMP,
-                    last_updated TEXT DEFAULT CURRENT_TIMESTAMP,
-                    FOREIGN KEY (patient_id) REFERENCES patient(id),
-                    FOREIGN KEY (split_definition_id) REFERENCES split_definitions(id)
-                );
-            """)
-
-            # Triggers to update last_updated field for all tables
-            tables = [
-                "manifest",
-                "patient",
-                "image",
-                "mask",
-                "image_annotations",
-                "image_labels",
-                "patient_labels",
-                "split_definitions",
-                "split",
-            ]
-
-            for table in tables:
-                cursor.execute(f"""
-                    CREATE TRIGGER IF NOT EXISTS update_{table}_last_updated
-                    AFTER UPDATE ON {table}
-                    FOR EACH ROW
-                    BEGIN
-                        UPDATE {table} SET last_updated = CURRENT_TIMESTAMP WHERE id = OLD.id;
-                    END;
-                """)
-
-    def insert_record(self, table: str, record: _BaseModel) -> None:
-        data = record.dict()
-        assert "created" not in data
-        assert "last_updated" not in data
-
-        with self._connect() as connection:
-            columns = ', '.join(data.keys())
-            placeholders = ', '.join(['?'] * len(data))
-            sql = f"INSERT INTO {table} ({columns}) VALUES ({placeholders})"
-            connection.execute(sql, tuple(data.values()))
+    image = relationship("Image", back_populates="labels")
 
 
-def populate_with_dummies(db_manager: DatabaseManager) -> None:
-    manifest = Manifest(id=1, name="Dummy Manifest")
-    db_manager.insert_record("manifest", manifest)
+class PatientLabels(Base):
+    __tablename__ = "patient_labels"
+    id = Column(Integer, primary_key=True)
+    key = Column(String)
+    value = Column(String)
+    patient_id = Column(Integer, ForeignKey("patient.id"))
 
-    split_definitions = SplitDefinitions(id=1, version="v1", description="Dummy Split")
-    db_manager.insert_record("split_definitions", split_definitions)
+    patient = relationship("Patient", back_populates="labels")
 
-    for i in range(101):
-        patient_code = f"A_{i:03}"
-        patient = Patient(id=i + 1, patient_code=patient_code, manifest_id=1)
-        db_manager.insert_record("patient", patient)
 
-        patient_age = random.randint(0, 100)
-        patient_label = PatientLabels(id=i + 1, key="age", value=str(patient_age), patient_id=patient.id)
-        db_manager.insert_record("patient_labels", patient_label)
+class SplitDefinitions(Base):
+    __tablename__ = "split_definitions"
+    id = Column(Integer, primary_key=True)
+    version = Column(String, nullable=False)
+    description = Column(String)
+    splits = relationship("Split", back_populates="split_definition")
 
-        for j in range(2):  # For two images per patient
-            image_identifier = f"Image_{patient_code}_{j}.jpg"
 
-            image = Image(id=2 * i + j + 1, filename=image_identifier, reader="OPENSLIDE", patient_id=patient.id)
-            db_manager.insert_record("image", image)
+class Split(Base):
+    __tablename__ = "split"
 
-            annotation_filename = f"{image_identifier}_annotation.json"
-            annotation = ImageAnnotations(id=2 * i + j + 1, filename=annotation_filename, reader="JSON",
-                                          image_id=image.id)
-            db_manager.insert_record("image_annotations", annotation)
+    id = Column(Integer, primary_key=True)
+    category: Column = Column(Enum(CategoryEnum), nullable=False)
+    patient_id = Column(Integer, ForeignKey("patient.id"))
 
-            label_value = random.choice(["cancer", "benign"])
-            label = ImageLabels(id=2 * i + j + 1, key="diagnosis", value=label_value, image_id=image.id)
-            db_manager.insert_record("image_labels", label)
+    patient = relationship("Patient", back_populates="split")
+    split_definition_id = Column(Integer, ForeignKey("split_definitions.id"))
 
-            mask_filename = f"{image_identifier}_mask.json"
-            mask = Mask(id=2 * i + j + 1, filename=mask_filename, reader="GEOJSON", image_id=image.id)
-            db_manager.insert_record("mask", mask)
+    split_definition = relationship("SplitDefinitions", back_populates="splits")
 
-        if i <= 30:
-            split_category = "train"
-        elif 31 <= i <= 60:
-            split_category = "validate"
-        else:
-            split_category = "test"
 
-        split = Split(id=i + 1, patient_id=patient.id, split_definition_id=1, category=split_category)
-        db_manager.insert_record("split", split)
+DATABASE_URL = "sqlite:///your_database_path_here.db"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+
+def insert_record(record):
+    with SessionLocal() as session:
+        session.add(record)
+        session.commit()
+
+
+def populate_with_dummies():
+    with SessionLocal() as session:
+        manifest = Manifest(name="Dummy Manifest")
+        session.add(manifest)
+        session.flush()
+
+        split_definition = SplitDefinitions(version="v1", description="Initial split")
+        session.add(split_definition)
+        session.flush()
+
+        for i in range(101):
+            patient_code = f"A_{i:03}"
+            patient = Patient(patient_code=patient_code, manifest=manifest)
+            session.add(patient)
+            session.flush()
+
+            patient_age = random.randint(0, 100)
+            patient_label = PatientLabels(key="age", value=str(patient_age), patient=patient)
+            session.add(patient_label)
+
+            for j in range(2):  # For two images per patient
+                image_identifier = f"Image_{patient_code}_{j}.jpg"
+                image = Image(filename=image_identifier, reader="OPENSLIDE", patient=patient)
+                session.add(image)
+                session.flush()  # Flush so that Image ID is populated for future records
+
+                mask_filename = f"{image_identifier}_mask.json"
+                mask = Mask(filename=mask_filename, reader="GEOJSON", image=image)
+                session.add(mask)
+
+                annotation_data = f"Annotation data for {image_identifier}"  # This is just dummy data
+                image_annotation = ImageAnnotations(annotation_data=annotation_data, image=image)
+                session.add(image_annotation)
+
+                label_data = (
+                    "cancer" if random.choice([True, False]) else "benign"
+                )  # Randomly decide if it's cancer or benign
+                image_label = ImageLabels(label_data=label_data, image=image)
+                session.add(image_label)
+
+            # Assigning splits for each patient
+            if i <= 30:
+                split_category = "train"
+            elif 31 <= i <= 60:
+                split_category = "validate"
+            else:
+                split_category = "test"
+
+            split = Split(category=split_category, patient=patient, split_definition=split_definition)
+            session.add(split)
+
+        session.commit()  # Commit all changes to the database
 
 
 if __name__ == "__main__":
-    db_path = Path("your_database_path_here.db")
-    db_manager = DatabaseManager(db_path)
-    db_manager.create_tables()
-    populate_with_dummies(db_manager)
+    create_tables()
+    populate_with_dummies()
