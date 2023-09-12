f539e1d5a57c58f5ae02554f96135db901f5f98b
Jonas Teuwen <j.teuwen@nki.nl>
2023-09-12 15:25:21 +0200

    Add h5 converter

--- ahcore/writers.py
+++ ahcore/writers.py
@@ -25,6 +25,7 @@ class H5FileImageWriter:
         tile_size: tuple[int, int],
         tile_overlap: tuple[int, int],
         num_samples: int,
+        is_binary: bool = False,
         progress: Optional[Any] = None,
     ) -> None:
         self._grid: Optional[Grid] = None
@@ -33,10 +34,11 @@ class H5FileImageWriter:
         self._size: tuple[int, int] = size
         self._mpp: float = mpp
         self._tile_size: tuple[int, int] = tile_size
-        self._tile_overlap: Tuple[int, int] = tile_overlap
+        self._tile_overlap: tuple[int, int] = tile_overlap
         self._num_samples: int = num_samples
+        self._is_binary: bool = is_binary
         self._progress = progress
-        self._image_dataset: Optional[h5py.Dataset] = None
+        self._data: Optional[h5py.Dataset] = None
         self._coordinates_dataset: Optional[h5py.Dataset] = None
         self._tile_indices: Optional[h5py.Dataset] = None
         self._current_index: int = 0
@@ -78,13 +80,22 @@ class H5FileImageWriter:
         # Initialize to -1, which is the default value
         self._tile_indices[:] = -1
 
-        self._image_dataset = h5file.create_dataset(
-            "data",
-            shape=(self._num_samples,) + batch_shape[1:],
-            dtype=batch_dtype,
-            compression="gzip",
-            chunks=(1,) + batch_shape[1:],
-        )
+        if not self._is_binary:
+            self._data = h5file.create_dataset(
+                "data",
+                shape=(self._num_samples,) + batch_shape[1:],
+                dtype=batch_dtype,
+                compression="gzip",
+                chunks=(1,) + batch_shape[1:],
+            )
+        else:
+            dt = h5py.vlen_dtype(np.dtype("uint8"))  # Variable-length uint8 data type
+            self._data = h5file.create_dataset(
+                "data",
+                shape=(self._num_samples,),
+                dtype=dt,
+                chunks=(1,),
+            )
 
         # This only works when the mode is 'overflow' and in 'C' order.
         metadata = {
@@ -106,21 +117,21 @@ class H5FileImageWriter:
     def consume(
         self,
         batch_generator: Generator[tuple[np.ndarray, np.ndarray], None, None],
-        connection_to_parent: Connection,
+        connection_to_parent: Optional[Connection] = None,
     ) -> None:
         """Consumes tiles one-by-one from a generator and writes them to the h5 file."""
         grid_counter = 0
-        # Mostly for mypy
-
-        assert self._grid, "Grid is not initialized"
-        assert self._tile_indices, "Tile indices are not initialized"
-        assert self._image_dataset, "Image dataset is not initialized"
-        assert self._coordinates_dataset, "Coordinates dataset is not initialized"
 
         try:
             with h5py.File(self._filename.with_suffix(".h5.partial"), "w") as h5file:
                 first_coordinates, first_batch = next(batch_generator)
                 self.init_writer(first_batch, h5file)
+
+                # Mostly for mypy
+                assert self._grid, "Grid is not initialized"
+                assert self._tile_indices, "Tile indices are not initialized"
+                assert self._data, "Dataset is not initialized"
+                assert self._coordinates_dataset, "Coordinates dataset is not initialized"
 
                 batch_generator = self._batch_generator((first_coordinates, first_batch), batch_generator)
                 # progress bar will be used if self._progress is not None
@@ -141,19 +152,21 @@ class H5FileImageWriter:
                         grid_counter += 1
 
                     batch_size = batch.shape[0]
-                    self._image_dataset[self._current_index : self._current_index + batch_size] = batch
+                    self._data[self._current_index : self._current_index + batch_size] = batch
                     self._coordinates_dataset[self._current_index : self._current_index + batch_size] = coordinates
                     self._current_index += batch_size
 
         except Exception as e:
             self._logger.error("Error in consumer thread for %s: %s", self._filename, exc_info=e)
-            connection_to_parent.send((False, self._filename, e))  # Send a message to the parent
+            if connection_to_parent:
+                connection_to_parent.send((False, self._filename, e))  # Send a message to the parent
         else:
             # When done writing rename the file.
             self._filename.with_suffix(".h5.partial").rename(self._filename)
         finally:
-            connection_to_parent.send((True, None, None))
-            connection_to_parent.close()
+            if connection_to_parent:
+                connection_to_parent.send((True, None, None))
+                connection_to_parent.close()
 
     @staticmethod
     def _batch_generator(first_coordinates_batch, batch_generator):
